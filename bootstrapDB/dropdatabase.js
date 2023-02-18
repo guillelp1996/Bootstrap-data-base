448dfc233bcff81e6d7c3c6b97686c60d83f1046
guillelp1996 <guillermovaldez8@gmail.com>
2023-02-18 13:11:08 +0100

    add delete a database by name

---
+++ bootstrapDB/dropdatabase.js
@@ -0,0 +1,20 @@
+"use strict"
+require("dotenv").config();
+const {DATABASE_NAME}= process.env;
+const getPool = require("./getPool");
+
+const dropDB = async ()=>{
+    try {
+        const pool = getPool();
+        await pool.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME};`);
+        console.log(`Database: ${DATABASE_NAME} was removed`);
+        console.assert(false, "removed")
+
+    } catch (error) {
+        console.log(error)
+    }finally{
+        process.exit()
+    }
+}
+
+dropDB()
